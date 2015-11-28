6fb8e16c389e707ff276e6db333d66b2e53ed105
Ashkon Farhangi <ashkon.f@gmail.com>
2015-11-27 16:40:31 -0800

    Fixed stopword removal.

--- ashlib/ling/stem.py
+++ ashlib/ling/stem.py
@@ -17,7 +17,7 @@ def lemmatize(words):
     for index, word in enumerate(words):
         try:
             ## TODO: might be missing some wordnet POS tags, and also might be bad even if not
-            tag = ashlib.ling.pos.standardizeWordnet(tags[index])
+            tag = pos.standardizeWordnet(tags[index])
             words[index] = LEMMATIZER.lemmatize(word, tag)
         except ValueError as error:
             words[index] = LEMMATIZER.lemmatize(word)
